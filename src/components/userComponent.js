6cd4a1ab7b126eb83568ecd50c8e76847b060178
Ahmet Cetintas <ahmetcetintas@outlook.com>
2021-11-09 00:24:56 +0300

    3. ders sonu

---
+++ src/components/userComponent.js
@@ -0,0 +1,21 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
+import User from "../models/user.js";
+import UserService from "../services/userService.js";
+
+console.log("userComponent loaded")
+
+let logger1 = new MongoLogger();
+let userService = new UserService(logger1)
+
+let user1 = new User(1, "Ahmet", "Çetintaş", "Aydın")
+let user2 = new User(2, "Engin", "Demiroğ", "Ankara")
+
+userService.add(user1)
+userService.add(user2)
+
+userService.getById(1)
+userService.list()
+
+console.log(userService.list());
+console.log(userService.getById(2).firstName);
+
